6f67df1f1a4bee490565a42f632dbc42f55f7062
Matthijs Brouwer <matthijs@eight.nl>
2023-03-29 14:15:49 +0200

    test with size and version

--- src/components/OpenAIAssetSource.tsx
+++ src/components/OpenAIAssetSource.tsx
@@ -9,8 +9,8 @@ async function fetchImage(prompt: string, API_KEY: string) {
 
   const raw = JSON.stringify({
     prompt,
-    n: 1,
-    size: '512x512',
+    n: 2,
+    size: '1024x1024',
     // eslint-disable-next-line camelcase
     response_format: 'b64_json',
   })
