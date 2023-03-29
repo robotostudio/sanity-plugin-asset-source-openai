42dd7c4ab89b99904b8eddb802994d584d5eb55f
Matthijs Brouwer <matthijs@artandflywork.org>
2023-03-29 14:39:44 +0200

    reset

--- src/components/OpenAIAssetSource.tsx
+++ src/components/OpenAIAssetSource.tsx
@@ -1,63 +1,73 @@
-import {AssetFromSource, AssetSourceComponentProps} from 'sanity'
-import {Dialog, Flex, Spinner, Stack, TextInput, Button, Label} from '@sanity/ui'
-import React, {useState} from 'react'
+import { AssetFromSource, AssetSourceComponentProps } from "sanity";
+import {
+  Dialog,
+  Flex,
+  Spinner,
+  Stack,
+  TextInput,
+  Button,
+  Label,
+} from "@sanity/ui";
+import React, { useState } from "react";
 
 async function fetchImage(prompt: string, API_KEY: string) {
-  const myHeaders = new Headers()
-  myHeaders.append('Authorization', `Bearer ${API_KEY}`)
-  myHeaders.append('Content-Type', 'application/json')
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${API_KEY}`);
+  myHeaders.append("Content-Type", "application/json");
 
   const raw = JSON.stringify({
     prompt,
-    n: 2,
-    size: '1024x1024',
+    n: 1,
+    size: "512x512",
     // eslint-disable-next-line camelcase
-    response_format: 'b64_json',
-  })
+    response_format: "b64_json",
+  });
 
   const response = await (
-    await fetch('https://api.openai.com/v1/images/generations', {
-      method: 'POST',
+    await fetch("https://api.openai.com/v1/images/generations", {
+      method: "POST",
       headers: myHeaders,
       body: raw,
-      redirect: 'follow',
+      redirect: "follow",
     })
-  ).json()
+  ).json();
 
-  return response.data
+  return response.data;
 }
 
-export default function ImagePlugin(props: AssetSourceComponentProps & {API_KEY: string}) {
-  const [prompt, setPrompt] = useState('')
-  const API_KEY = props.API_KEY
-  const [loading, setLoading] = useState(false)
-  const [img, setImg] = useState('')
+export default function ImagePlugin(
+  props: AssetSourceComponentProps & { API_KEY: string }
+) {
+  const [prompt, setPrompt] = useState("");
+  const API_KEY = props.API_KEY;
+  const [loading, setLoading] = useState(false);
+  const [img, setImg] = useState("");
 
   async function generateImage() {
-    setLoading(true)
-    const image = await fetchImage(prompt, API_KEY)
-    const newImageValue = `data:image/png;base64,${image[0].b64_json}`
-    setImg(newImageValue)
-    setLoading(false)
+    setLoading(true);
+    const image = await fetchImage(prompt, API_KEY);
+    const newImageValue = `data:image/png;base64,${image[0].b64_json}`;
+    setImg(newImageValue);
+    setLoading(false);
   }
 
   function oonfirmImage() {
     const asset: AssetFromSource = {
-      kind: 'url',
+      kind: "url",
       value: img,
       assetDocumentProps: {
-        _type: 'sanity.imageAsset',
+        _type: "sanity.imageAsset",
         description: prompt,
       } as any,
-    }
-    props.onSelect([asset])
+    };
+    props.onSelect([asset]);
   }
 
   function handleClose() {
-    props.onClose()
+    props.onClose();
   }
   function handlePromptChange(event: any) {
-    setPrompt(event.target.value)
+    setPrompt(event.target.value);
   }
   return (
     <>
@@ -69,7 +79,7 @@ export default function ImagePlugin(props: AssetSourceComponentProps & {API_KEY:
         width={4}
       >
         <Stack padding={4}>
-          <Flex direction={'column'} gap={2}>
+          <Flex direction={"column"} gap={2}>
             <Label>Enter Prompt Text</Label>
             <TextInput
               placeholder="Cats Driving Boat"
@@ -96,5 +106,5 @@ export default function ImagePlugin(props: AssetSourceComponentProps & {API_KEY:
         </Stack>
       </Dialog>
     </>
-  )
+  );
 }
